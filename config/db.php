6adef37bb36e0e5878fa82e9da5dfcfbf81d6118
linkuha <linkuha@gmail.com>
2015-12-20 04:21:37 +0600

    First commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2_first',
+    'username' => 'yii2_first',
+    'password' => 'n6BS1b3Nzb',
+    'charset' => 'utf8',
+];
---
+++ config/params.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'adminEmail' => 'hostmaster@pudich.ru',
+];
